e40ce85b401b86f1b3e83f57abdf8baae8583079
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-01 13:43:40 -0600

    Uploaded for review

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -12,15 +12,22 @@ namespace ConsoleAppAssignment
         {
             // This asks the user to enter a number
             // then multiplies it and prints result to console
-            Console.WriteLine("Enter a number from 1 to 100,000,000 and press Enter.");
-            // **** uint firstNumber = Convert.ToString(Console.ReadLine());
+            uint firstNumber, secondNumber;
+            Console.WriteLine("Enter a number from 1 to 10,000,000 and press Enter.");
+            uint firstNumber = (uint)Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Let's multiply " + firstNumber + " by 50!");
-            Console.WriteLine(firstNumber + 50);
+            Console.WriteLine(firstNumber * 50);
             Console.ReadLine();
 
             Console.WriteLine("Let's add 25 to that and see what we get!\nPress Enter to continue.");
-            uint secondNumber = firstNumber + 75;
+            secondNumber = firstNumber + 25;
             Console.WriteLine(secondNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Let's enter a new number with a decimal, and divide it by 12.5!\nPress Enter to continue.");
+            decimal thirdNumber = Console.ReadLine();
+            decimal result1 = thirdNumber / 12.5m;
+            Console.WriteLine(result1);
             Console.ReadLine();
 
 
@@ -28,8 +35,12 @@ namespace ConsoleAppAssignment
 
 
 
+            // Console.WriteLine("Hello World!");
+        }
 
-            Console.WriteLine("Hello World!");
+        private static void WriteLine(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
