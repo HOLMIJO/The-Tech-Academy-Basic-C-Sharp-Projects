f55a5adbfb19f27fee6b1bcfa62c9a45736b7811
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-01 14:14:53 -0600

    Review broken code

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleAppAssignment
         {
             // This asks the user to enter a number
             // then multiplies it and prints result to console
-            uint firstNumber, secondNumber;
             Console.WriteLine("Enter a number from 1 to 10,000,000 and press Enter.");
             uint firstNumber = (uint)Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Let's multiply " + firstNumber + " by 50!");
@@ -20,13 +19,13 @@ namespace ConsoleAppAssignment
             Console.ReadLine();
 
             Console.WriteLine("Let's add 25 to that and see what we get!\nPress Enter to continue.");
-            secondNumber = firstNumber + 25;
+            uint secondNumber = (uint)Convert.ToInt32(Console.ReadLine()) + 25;
             Console.WriteLine(secondNumber);
             Console.ReadLine();
 
             Console.WriteLine("Let's enter a new number with a decimal, and divide it by 12.5!\nPress Enter to continue.");
-            decimal thirdNumber = Console.ReadLine();
-            decimal result1 = thirdNumber / 12.5m;
+            decimal thirdNumber = (decimal)Convert.ToInt32(Console.ReadLine());
+            decimal result1 = (decimal)thirdNumber / 12.5m;
             Console.WriteLine(result1);
             Console.ReadLine();
 
