717a70d7984104a702720c135d592d000dc1ccab
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-01 19:04:19 -0600

    For review of modulus operation

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -10,36 +10,44 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
+            //// This asks the user to enter a number
+            //// then multiplies it and prints result to console
+            //Console.WriteLine("Enter a number from 1 to 10,000,000 and press Enter.");
+            //uint firstNumber = (uint)Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Let's multiply " + firstNumber + " by 50!");
+            //Console.WriteLine(firstNumber * 50);
+            //Console.ReadLine();
+
+            //// This asks the user to enter a number
+            //// then divides it by 12.5 and prints result to console
+            //Console.WriteLine("Enter another number from 1 to 10,000 and press Enter.");
+            //decimal secondNumber = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("Let's divide the " + secondNumber + " by 12.5!");
+            //Console.WriteLine(secondNumber / 12.5m);
+            //Console.ReadLine();
+
+            //// This asks the user to enter a number
+            //// then checks to see if number is greater than 50
+            //// and prints boolean result to console. Only true or false will appear.
+            //Console.WriteLine("Enter another whole number and press Enter.");
+            //int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your number was: " + thirdNumber);
+            //Console.ReadLine();
+            //Console.WriteLine("If the number you added is greater than 50\nThe result will show as \"True\",\nOtherwise it will display as \"False\".");
+            //int bigSmall = (int)thirdNumber;
+            //int basePick = 50;
+            //bool isNot = bigSmall >= basePick;
+            //Console.WriteLine(isNot);
+            //Console.ReadLine();
+
             // This asks the user to enter a number
-            // then multiplies it and prints result to console
-            Console.WriteLine("Enter a number from 1 to 10,000,000 and press Enter.");
-            uint firstNumber = (uint)Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Let's multiply " + firstNumber + " by 50!");
-            Console.WriteLine(firstNumber * 50);
+            // then divides it by 7 and prints result to console
+            Console.WriteLine("Enter another number from 1 to 10,000 and press Enter.");
+            int fourthNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Let's divide the " + fourthNumber + " by 7!");
+            Console.WriteLine("The result is: " + fourthNumber % 7);
+            // Console.WriteLine(fourthNumber % 7);
             Console.ReadLine();
-
-            Console.WriteLine("Let's add 25 to that and see what we get!\nPress Enter to continue.");
-            uint secondNumber = (uint)Convert.ToInt32(Console.ReadLine()) + 25;
-            Console.WriteLine(secondNumber);
-            Console.ReadLine();
-
-            Console.WriteLine("Let's enter a new number with a decimal, and divide it by 12.5!\nPress Enter to continue.");
-            decimal thirdNumber = (decimal)Convert.ToInt32(Console.ReadLine());
-            decimal result1 = (decimal)thirdNumber / 12.5m;
-            Console.WriteLine(result1);
-            Console.ReadLine();
-
-
-
-
-
-
-            // Console.WriteLine("Hello World!");
-        }
-
-        private static void WriteLine(string v)
-        {
-            throw new NotImplementedException();
         }
     }
 }
