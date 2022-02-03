0fcbfb414594ce70821a0d7119452df7b955dc8a
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-03 12:42:35 -0600

    Uploaded revised code

--- ConditionalStatementChallenge/ConditionalStatementChallenge/Program.cs
+++ ConditionalStatementChallenge/ConditionalStatementChallenge/Program.cs
@@ -6,40 +6,39 @@ namespace ConditionalStatementChallenge
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
+            
             Console.WriteLine("Hello World! Please enter your name.");
-            Convert.ToString(Console.ReadLine());
-            Console.ReadLine();
+            string name = Console.ReadLine();
             
 
             if (name == "Joseph")
             {
-                Console.WriteLine("Your name is Joseph!");
+                Console.WriteLine("Hello, " + name + " welcome to the Conditional Statement Challenge!");
                 Console.ReadLine();
             }
             else if (name == "Brandon")
             {
-                Console.WriteLine("Your name is NOT Joseph!");
+                Console.WriteLine("Hello, " + name + " welcome to the Conditional Statement Challenge!");
                 Console.ReadLine();
             }
             else if (name == "Becca")
             {
-                Console.WriteLine("Your name is NOT Joseph!");
+                Console.WriteLine("Hello, " + name + " welcome to the Conditional Statement Challenge!");
                 Console.ReadLine();
             }
             else if (name == "Stephen")
             {
-                Console.WriteLine("Your name is NOT Joseph!");
+                Console.WriteLine("Hello, " + name + " welcome to the Conditional Statement Challenge!");
                 Console.ReadLine();
             }
             else if (name == "Bill")
             {
-                Console.WriteLine("Your name is NOT Joseph!");
+                Console.WriteLine("Hello, " + name + " welcome to the Conditional Statement Challenge!");
                 Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Your name is neither Brandon, Becca, Stephen, or Bill!");
+                Console.WriteLine("Sorry, Your name is neither Joseph, Brandon, Becca, Stephen, or Bill!");
                 Console.ReadLine();
             }
         }
