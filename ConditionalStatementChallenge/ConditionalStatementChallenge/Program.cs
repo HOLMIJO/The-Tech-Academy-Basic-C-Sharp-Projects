f48dec1a6dc7afb3a8e3a99bb8b4468b126e0596
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-03 12:03:05 -0600

    Uploaded for review

---
+++ ConditionalStatementChallenge/ConditionalStatementChallenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConditionalStatementChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World! Please enter your name.");
+            Convert.ToString(Console.ReadLine());
+            Console.ReadLine();
+            
+
+            if (name == "Joseph")
+            {
+                Console.WriteLine("Your name is Joseph!");
+                Console.ReadLine();
+            }
+            else if (name == "Brandon")
+            {
+                Console.WriteLine("Your name is NOT Joseph!");
+                Console.ReadLine();
+            }
+            else if (name == "Becca")
+            {
+                Console.WriteLine("Your name is NOT Joseph!");
+                Console.ReadLine();
+            }
+            else if (name == "Stephen")
+            {
+                Console.WriteLine("Your name is NOT Joseph!");
+                Console.ReadLine();
+            }
+            else if (name == "Bill")
+            {
+                Console.WriteLine("Your name is NOT Joseph!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your name is neither Brandon, Becca, Stephen, or Bill!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
