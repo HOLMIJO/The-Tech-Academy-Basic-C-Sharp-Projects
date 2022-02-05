0c2ca8589b970cad70f169307035b9ad2afd0e86
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-05 13:26:28 -0600

    Updated code added while loop

--- ConsoleAppSubmissionAssignment2/ConsoleAppSubmissionAssignment2/Program.cs
+++ ConsoleAppSubmissionAssignment2/ConsoleAppSubmissionAssignment2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Loops
+namespace ConsoleAppSubmissionAssignment2
 {
     class Program
     {
@@ -12,7 +12,6 @@ namespace Loops
         {
             // User is greeted and asked to pick a number between 1-10
             Console.WriteLine("I'm thinking of a secret number! Can you find it?\nPick a number between 1 and 10.");
-            Console.ReadLine();
             // User input which is converted to an integer and the value is stored.
             int numberPicked = Convert.ToInt32(Console.ReadLine());
             // The variable for the Secret Number is being compared as True equals number 8.
@@ -50,6 +49,22 @@ namespace Loops
             while (!SecretNumber);
 
             Console.Read();
+            // This is the start of the while loop. Here we are starting at a count of zero.
+            // As long as the count is less than 25, the count will increment by one until 
+            // the count increases to 25. Once the count reaches 25, the message will display and the program ends.
+            int count = 0;
+            while (count < 25)
+            {
+                // The count will display as it progresses.
+                Console.WriteLine(count);
+                count++; // This is where we tell the count to incrementally increase by one.
+                Console.Read();
+                
+            }
+            // This message will display once the count has met it's limits.
+            Console.WriteLine("That's all she wrote! Goodbye!");
+            Console.ReadLine();
         }
+
     }
 }
