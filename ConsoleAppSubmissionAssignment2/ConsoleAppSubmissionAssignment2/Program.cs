0892769d4fac7c815b36ee17bf2a7120fa9bc0bf
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-04 23:20:35 -0600

    Uploaded for review

--- ConsoleAppSubmissionAssignment2/ConsoleAppSubmissionAssignment2/Program.cs
+++ ConsoleAppSubmissionAssignment2/ConsoleAppSubmissionAssignment2/Program.cs
@@ -1,37 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleAppSubmissionAssignment2
+namespace Loops
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // User is greeted with overview and asked to press Enter to continue.
-            Console.WriteLine("Hello World, Let's get those numbers rolling\nCounting from 1 to 25.\nPress ENTER to start!");
+            // User is greeted and asked to pick a number between 1-10
+            Console.WriteLine("I'm thinking of a secret number! Can you find it?\nPick a number between 1 and 10.");
             Console.ReadLine();
-            // We're going to count from 1 ongoing. While the numbers are less than 25, they will incrementally count up.
-            // Once the increment reaches 25, the counting stops.
-            int index = 1;
-            while (index <= 25)
+            // User input which is converted to an integer and the value is stored.
+            int numberPicked = Convert.ToInt32(Console.ReadLine());
+            // The variable for the Secret Number is being compared as True equals number 8.
+            bool SecretNumber = numberPicked == 8;
+            // During the course of the user making choices, the user will be met with various messages based on their number choice.
+
+            do  // The Do statement fires off as long as the boolean SecretNumber value is False.
             {
-                Console.WriteLine(index); // The result of index is 1
-                index++; // index = 1 then increments by 1 onward.
+                // This is saying if the case number is input, run these commands or statements
+                switch (numberPicked)
+                {
+                    // Only the answers meeting these choices will get these messages.
+                    case 7: // Only if the user chooses the number 7 will they get this message.
+                        Console.WriteLine("You guessed 7. You're getting warmer. Try again.");
+                        Console.WriteLine("Guess another number?");
+                        numberPicked = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 10: // Only if the user chooses the number 10 will they get this message.
+                        Console.WriteLine("You guessed 10. So close! Try again.");
+                        Console.WriteLine("Guess another number?");
+                        numberPicked = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8: // Only if the user chooses the number 8 will they get this message.
+                        Console.WriteLine("You guessed the number 8. That is correct!\nYou guessed the Secret Number!");
+                        SecretNumber = true;
+                        break;
+                    default: // This is the default answer given for most wrong answers including choices above number 10 and beyond.
+                        Console.WriteLine("You're not very good at this. Try again!");
+                        Console.WriteLine("Guess another number?");
+                        numberPicked = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
             }
-            // Message displays once the incremental count reaches 25.
-            Console.WriteLine("That was fun, let's do it all again!\nPress ENTER to continue.");
-            Console.ReadLine();
-            // We start the counting over. Index is set to 1.
-            index = 26;
-            // Do count from 1 onwards incrementing by 1.
-            do
-            {
-                // Write the index of 1, incrementing by 1, as long as the indexed incremented is 25 or less.
-                Console.WriteLine(index);
-                index++;
-            } while (index <= 50);
-            // The closing message is displayed.
-            Console.WriteLine("Now the fun is done!");
-            Console.ReadLine();
+            // This is saying While the SecretNumber is False, run the Do Statement
+            while (!SecretNumber);
+
+            Console.Read();
         }
     }
 }
