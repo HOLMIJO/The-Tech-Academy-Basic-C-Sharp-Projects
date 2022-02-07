27b6394756697f536fdb41e17a26a8d76aebba16
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-07 17:47:36 -0600

    Uploaded for review purposes only

---
+++ ConsoleAppArraySubmissionAssignment/ConsoleAppArraySubmissionAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter \"0\" to display the Student's Name.\nEnter \"1\" to display the school name.\nEnter \"2\" to display current course.");
+        Console.ReadLine();
+        string[] strArray1 = new string[] { "Joseph Holmin", "The Tech Academy", "Software Development Bootcamp" };
+        Console.WriteLine(strArray1[]);
+        Console.ReadLine();
+
+
+        
+    }
+}
