60c71102faf6b24df480ff66539fa792b38e67ac
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-10 16:41:07 -0600

    Uploaded strictly for review

--- ConsoleAppMultiPartAssignment/ConsoleAppMultiPartAssignment/Program.cs
+++ ConsoleAppMultiPartAssignment/ConsoleAppMultiPartAssignment/Program.cs
@@ -23,26 +23,69 @@ namespace ConsoleAppMultiPartAssignment
             }
             Console.ReadLine();
 
-            //for (int a = 0; a < 25; a--)
-            //{
-            //    Console.WriteLine("value : {0}", a);
-            //}
-            //Console.ReadLine();
+            // This is an infinite loop.
+            for ( ; ; )
+            {
+                Console.Write("x");
+            }
+            Console.ReadLine();
 
-
+            // Here c will print it's value
+            // as it increments by 1 
+            // until it meets the limit of 5
             for (int c = 0; c < 5; c++)
             {
                 Console.WriteLine("Value of c: {0}", c);
             }
             Console.ReadLine();
-            
 
+            // Here b will print it's value
+            // as it increments by 1 
+            // until it is less than or equal to 5
             for (int b = 0; b <= 5; b++)
             {
                 Console.WriteLine("b value: {0}", b);
             }
             Console.ReadLine();
 
+            // A string of unique values
+            List<string> profileList = new List<string>();
+            profileList.Add("Name: Joseph Holmin");
+            profileList.Add("Phone Number: +1(901) 677-4221");
+            profileList.Add("Email: holminj@icloud.com");
+            profileList.Add("State: Arkansas");
+            profileList.Add("Country: USA");
+            // User is given instructions and prompted to enter specific search terms.
+            Console.WriteLine("Please enter a search term.\nYou may choose \"Name\", \"Phone Number\", \"Email\", \"State\", or \"Country\".\nPlease enter your selection now.");
+            string strList = Convert.ToString(Console.ReadLine()); //This line saves the input result
+
+            switch (profileList)
+            {
+                case "Name":
+                    Console.WriteLine("The answer is: " + profileList[strList]);
+                    break;
+
+                case "Phone Number":
+                    Console.WriteLine("The answer is: " + profileList[strList]);
+                    break;
+
+                case "Email":
+                    Console.WriteLine("The answer is: " + profileList[strList]);
+                    break;
+
+                case "State":
+                    Console.WriteLine("The answer is: " + profileList[strList]);
+                    break;
+
+                case "Country":
+                    Console.WriteLine("The answer is: " + profileList[strList]);
+                    break;
+
+                default:
+                    Console.WriteLine("Index is not available.");
+                    break;
+            }
+            Console.ReadLine();
 
 
         }
