19da40ef6ecf9b5e7245468c203f04184a72644f
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-12 16:30:27 -0600

    Uploaded solely for review

---
+++ StringsAndIntegersSubmission/StringsAndIntegersSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringsAndIntegersSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            intList.Add(8);
+            intList.Add(10);
+            intList.Add(4);
+            intList.Add(12);
+            intList.Add(7);
+            intList.Add(2);
+
+            Console.WriteLine("Pick a number.This number will be used to divide each number in the list.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            foreach (int i in intList)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.WriteLine(intList[i]);
+            }
+            Console.WriteLine("{0} / {1} = {2}", intList, userInput, intList / userInput);
+            Console.ReadLine();
+        }
+    }
+}
