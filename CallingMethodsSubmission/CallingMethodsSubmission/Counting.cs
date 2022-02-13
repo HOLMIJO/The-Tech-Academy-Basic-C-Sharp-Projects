0fe76e2be3bc3b7a0ff0f6fdb41e0369ec884bcc
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-13 13:43:49 -0600

    Initial commit

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Counting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class Counting
+    {
+        public int AddIt(int number) //Passes in number variable from Main program which is the 
+                                     //user input. 
+        {
+            int a = 25;
+            return number + a; // Returning this value to the part of the program that called it. 
+        }
+
+        public int SubIt(int number) //Passes in number variable from Main program which is the 
+                                     //user input. 
+        {
+            int b = 2;
+            return number - b; // Returning this value to the part of the program that called it. 
+        }
+
+        public int TimesIt(int number) //Passes in number variable from Main program which is the 
+                                       //user input. 
+        {
+            int c = 10;
+            return number * c; // Returning this value to the part of the program that called it. 
+        }
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Message for user to input number and to press enter.
+            Console.WriteLine("Greetings! What number would you like to do your math operations on?\nEnter a number to continue, and press ENTER.");
+            // This line stores user input and holds for math operations.
+            int number = Convert.ToInt32(Console.ReadLine());
+            // Message to user...
+            Console.WriteLine("I will now add 25 to your number.");
+            // This creates a new object of the Counting class and names it "Object".
+            Counting Object = new Counting();
+            // This variable return1 accesses the Counting class methods...
+            int return1 = Object.AddIt(number);
+            // This line prints the sum of the math operation.
+            Console.WriteLine(return1);
+            // Message to user...
+            Console.WriteLine("I will now subtract 2 from your number.");
+            // This variable return2 accesses the Counting class methods...
+            int return2 = Object.SubIt(number);
+            // This line prints the sum of the math operation.
+            Console.WriteLine(return2);
+            // Message to user...
+            Console.WriteLine("I will now multiply your number by 10.");
+            // This variable return3 accesses the Counting class methods...
+            int return3 = Object.TimesIt(number);
+            // This line prints the sum of the math operation.
+            Console.WriteLine(return3);
+            // Closing message to
+            Console.ReadLine();
+        }
+    }
+}
+
