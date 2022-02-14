4be4ab7d38762cd085bfd74495bc2043d249f83b
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-14 11:52:42 -0600

    Uploaded for review only

---
+++ MethodClassSubmission/MethodClassSubmission/MathOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    // Created Class of MathOps
+    public class MathOps
+    {
+        // The public void Addition method takes 2 integers as parameters
+        public void Addition(int num1, int num2)
+        {
+            // This sets an integer variable named sum,
+            // the first argument is determined in the
+            // main program and also assigns a placeholder for the 2nd argument named here.
+            int sum = num1 + num2;
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This sets a variable of addItUp and instantiates the MathOps method
+            var addItUp = new MathOps();
+            // This passes in two numbers
+            int num1 = 22;
+            int num2 = 25;
+            // This calls the method and specifies the parameters by name
+            addItUp.Addition(num1, num2);
+            // This displays the second integer to the screen.
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
