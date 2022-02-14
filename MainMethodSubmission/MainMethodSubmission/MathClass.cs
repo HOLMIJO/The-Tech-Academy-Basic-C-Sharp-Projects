aede1adabad68331190c5035781e7b44060160cd
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-13 18:37:01 -0600

    Initial Commit

---
+++ MainMethodSubmission/MainMethodSubmission/MathClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class MathClass  // Created MathClass for these math operations.
+    {
+        // This method will take a user input as an integer and returns an integer back.
+        public int AddMeUp(int a)  
+        {
+            // This takes the user input "a" and adds to it...
+            int inputA = a + 250;
+            //returned "inputA" value to main program.
+            return inputA;  
+        }
+
+        // This method has the same name as the one above. However this method is 
+        // slightly different as this one now takes in a decimal and returns a decimal.
+        public int AddMeUp(decimal b)  
+        {
+            // This takes the user input "b" and adds to it...
+            decimal inputB = b - 12.50m;
+            //returned "inputB" value to main program.
+            return Convert.ToInt32(inputB);
+        }
+
+        // This creates a new method taking user input as a string, converts to an integer
+        // before carrying out a math operation.
+        public int AddMeUp(string c)
+        {
+            // This converts user input of string into an integer, assigns as "inputC".
+            int inputC = Convert.ToInt32(c);
+            // Takes "inputC" multiplys by 24, and stores value, passing it back to the main program.
+            int inputD = inputC * 24; 
+            return inputD; 
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This instantiates a new object from the MathClass.
+            MathClass inputA = new MathClass();
+            // User is asked for an input of integer.
+            Console.WriteLine("Please enter a whole number for some addition fun!");
+            // This line stores the user input.
+            int a = Convert.ToInt32(Console.ReadLine());
+            // This calls the AddMeUp method.
+            int result1 = inputA.AddMeUp(a);
+            // This returns the sum and friendly message
+            Console.WriteLine("Your chosen number plus 250, equals: {0}.", result1);
+            Console.ReadLine();
+            // User is asked for an input of integer.
+            Console.WriteLine("Now let's enter a decimal that you would like to subtract 12.50 from.");
+            // This line stores the user input.
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+            // This calls the AddMeUp method.
+            int result2 = inputA.AddMeUp(b);
+            // This returns the sum and friendly message
+            Console.WriteLine("Your chosen number minus 12.50 equals: {0}.", result2);
+            Console.ReadLine();
+            // User is asked for an input of integer which can take in a string.
+            Console.WriteLine("Please type in another whole number.");
+            // This line stores the user input.
+            string c = Console.ReadLine();
+            // This calls the AddMeUp method.
+            int result3 = inputA.AddMeUp(c);
+            // This returns the sum and friendly message
+            Console.WriteLine("Your chosen number times 24 equals: {0}.", result3);
+            Console.ReadLine();
+        }
+    }
+}
