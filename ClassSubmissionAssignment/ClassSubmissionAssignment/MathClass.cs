af2ad658f74729ec294a2cd861b9718f77bff516
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-14 19:07:39 -0600

    Initial commit

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/MathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassSubmissionAssignment
+{
+    class MathClass
+    {
+        // This is a void method that outputs an integer
+        public void Divided(int input, out int sum)
+        {
+            // The sum varaible holds the division math operation
+            sum = input / 2;
+        }
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This instantiates the MathClass class.
+            MathClass math1 = new MathClass();
+            // Gives friendly message for user input.
+            Console.WriteLine("Please enter a number, then press ENTER.");
+            // This stores user input to a variable.
+            int input = Convert.ToInt32(Console.ReadLine());
+            // This variable holds the math operation
+            int sum;
+            // This calls on the input number and outputs the result
+            math1.Divided(input, out sum);
+            // This line prints a friendly message including the results.
+            Console.WriteLine("Your number divided by 2 equals: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
