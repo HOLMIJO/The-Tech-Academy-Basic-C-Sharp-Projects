939b96ec674a21345ba547063a221c65f32a9eb2
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-15 16:37:01 -0600

    Uploaded for review

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Becca", "Joe", "Brandon" };
+            //game.ListPlayers();
+            //Console.ReadLine();
+
+            List<string> players = new List<string>() { "Becca", "Joe", "Brandon" };
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+                Console.ReadLine();
+            }
+
+            //Deck Deck = new Deck();
+            //Deck.Shuffle(3);
+
+            //foreach (Card card in Deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(Deck.Cards.Count);
+            //Console.ReadLine();
+        }
+    }
+}
