8d1072ba61271f96fabf8d839521cd06f1855826
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-16 14:04:34 -0600

    Initial commit

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // This is the Employee class with a generic parameter type
+    public class Employee<T>
+    {
+        // This is a list with a generic type parameter
+        public List<T> things;
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // This instantiates the new Employee as a string
+            Employee<string> employee = new Employee<string>();
+            // This is a list of string with variable of thingys
+            List<string> thingys = new List<string>()
+            {
+                "House", "Ball", "Car"
+            };
+            // This takes from the Employee class and things method. 
+            employee.things = thingys;
+            // This says to print each item in the list to the console.
+            foreach (string thing in employee.things)
+            {
+                // This prints to console.
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
