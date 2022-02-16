5ac5bc7411f4f4d33f877b37cc128e6f08057b68
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-15 19:33:17 -0600

    Updated for Polymorphism Submission.

--- AbstractClassSubmission/AbstractClassSubmission/Employee.cs
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -6,13 +6,42 @@ using System.Threading.Tasks;
 
 namespace AbstractClassSubmission
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         // SayName method is implemented here, inherited properties from Person class.
         public override void SayName()
         {
             // Friendly message taking from the Person class through inheritance.
             Console.WriteLine("Greetings: {0} {1}!", firstName, lastName);
+        }
+
+
+        // Uses same method as IQuittable interface
+        public void Quit(Employee employee)
+        {
+            // Friendly greeting giving user instructions.
+            Console.WriteLine("Please advise your employment status.\nAre you a current employee? \nPlease enter y or n and press ENTER.");
+            // This stores the user input
+            string response = Console.ReadLine();
+            // If user answers y, they are given friendly greeting.
+            if (response == "y")
+            {
+                // Friendly response for user input of Y.
+                Console.WriteLine("Great, we are glad you are employed here!");
+            }
+            // If user answers n, they are given the following response.
+            if (response == "n")
+            {
+                // Friendly response based on user input of n.
+                Console.WriteLine("We are sorry to see you go. We wish you well in all of your future endeavors.");
+            }
+            // If user answers other than y or n, they are given the following response.
+            else
+            {
+                // Friendly response based on user input other than y.
+                Console.WriteLine("You have entered an invalid response.");
+            }
+            Console.ReadLine();
         }
     }
 }
--- AbstractClassSubmission/AbstractClassSubmission/Program.cs
+++ AbstractClassSubmission/AbstractClassSubmission/Program.cs
@@ -18,6 +18,18 @@ namespace AbstractClassSubmission
             employee.lastName = "Student";
             // This line calls the SayName method on the employee object.
             employee.SayName();
+            // This line instantiates a new employee from the Employee class. 
+            Employee employee1 = new Employee();
+            // This line from the employee method gives a first name as a string.
+            employee1.firstName = "Joseph";
+            // This line from the employee method gives a last name as a string.
+            employee1.lastName = "Holmin";
+            // This calls the Quit method on the new employee.
+            employee1.Quit(employee);
+            // The employee2 is created and has access to the IQuittable interface through polymorphism.
+            IQuittable employee2 = new Employee();
+            // This employee2 has access to the quit method.
+            employee2.Quit(employee);
         }
     }
 }
