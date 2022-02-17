0e8530d4087d678ba41864f4adf577a94b7807b1
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-16 20:00:28 -0600

    Initial commit

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // This is the class, now struct of Number
+    public struct Number
+    {
+        // Variable Amount of decimal type
+        public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // From Number struct BitCoin is the instantiated object.
+            Number BitCoin = new Number
+            {
+                Amount = 3250
+            };
+            // This line writes the BitCoint amount to console.
+            Console.WriteLine(BitCoin.Amount);
+        }
+    }
+}
