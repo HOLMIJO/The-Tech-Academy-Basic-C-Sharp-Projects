4f75d5eac24e6b24d06cac4a97c5fdc9f01f2d30
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-18 23:21:19 -0600

    Initial commit

---
+++ ConstructorSubmission/ConstructorSubmission/OurPups.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssignment
+{
+    public class K9
+    {
+        // Here we chain the two constructors together.
+        // This constructor helps to create a pup profile even before data is stored. 
+        // This will create placeholding properties and assign placeholding values that can de overriden later in the program.
+        public K9() : this(null, 0)
+        {
+        }
+        // This creates the characteristics of our pups. 
+        public K9(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // Variable Amount of string type
+        public string Name { get; set; }
+        // Variable Amount of integer type
+        public int Age { get; set; }
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // create a consant variable.
+            const string owner = "Stephen Patterson";
+            Console.WriteLine(owner);
+            // create a variable using the keyword "var"
+            var ourPups = new Dictionary<string, int>() { ["Taco"] = 1, ["Sweetie"] = 15, ["Skeeter"] = 9, ["Red"] = 12 };
+            // This prints a specific item, being the int of age from key in the dictionary.
+            Console.WriteLine(ourPups["Sweetie"]);
+            ConstructorSubmissionAssignment.K9 k9 = new ConstructorSubmissionAssignment.K9("Ace", 14);
+            Console.WriteLine(k9.Name + " " + k9.Age);
+            Console.ReadLine();
+            
+        }
+    }
+}
