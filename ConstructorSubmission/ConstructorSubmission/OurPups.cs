b87f7fbc9ebeed0f07b48a07e0d58671bfa4b4c6
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2022-02-19 16:05:45 -0600

    Updated namespaces

--- ConstructorSubmission/ConstructorSubmission/OurPups.cs
+++ ConstructorSubmission/ConstructorSubmission/OurPups.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConstructorSubmissionAssignment
+namespace ConstructorSubmission
 {
     public class K9
     {
--- ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -17,7 +17,7 @@ namespace ConstructorSubmission
             var ourPups = new Dictionary<string, int>() { ["Taco"] = 1, ["Sweetie"] = 15, ["Skeeter"] = 9, ["Red"] = 12 };
             // This prints a specific item, being the int of age from key in the dictionary.
             Console.WriteLine(ourPups["Sweetie"]);
-            ConstructorSubmissionAssignment.K9 k9 = new ConstructorSubmissionAssignment.K9("Ace", 14);
+            ConstructorSubmission.K9 k9 = new ConstructorSubmission.K9("Ace", 14);
             Console.WriteLine(k9.Name + " " + k9.Age);
             Console.ReadLine();
             
